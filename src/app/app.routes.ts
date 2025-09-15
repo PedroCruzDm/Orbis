c9bcf243a7cb9d071a274ab4ce1625e893e96809
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2025-09-15 16:41:07 -0300

    configurando rotas do projeto Orbis

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/HomePage/app.HomePage';
+import { CadastroComponent } from './components/Cadastro/app.Cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'cadastro',
+        component: CadastroComponent
+    },
+    {
+        path: 'login',
+        component: CadastroComponent
+    },
+    {
+        path: 'dashboard',
+        component: CadastroComponent
+    },
+    {
+        path: 'profile',
+        component: CadastroComponent
+    },
+    {
+        path: 'settings',
+        component: CadastroComponent
+    },
+    {
+        path: 'calendario',
+        component: HomeComponent
+    },
+    {
+        path: 'ranking',
+        component: HomeComponent
+    },
+    {
+        path: 'foco',
+        component: HomeComponent
+    }
+];
---
+++ src/app/components/Cadastro/app.Cadastro.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<h1>Cadastro Page</h1> <br> <p>Esta é a página de cadastro.</p>`
+})
+export class CadastroComponent {}
